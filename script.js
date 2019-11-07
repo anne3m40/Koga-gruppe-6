c12d2cfb851955c8296cf169603567b0b2143568
Christina Jørgensen <christina@desktop-gs0tdbk.kea.local>
2019-11-07 15:45:07 +0100

    Burger er sat ind på cykelsiden samt integreret i script

--- script.js
+++ script.js
@@ -11,6 +11,32 @@ function start() {
     hentJson();
     const filterKnapper = document.querySelectorAll(".filterknapper");
     filterKnapper.forEach(knap => knap.addEventListener("click", filtrerCykler));
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+    document.querySelector("#menuknapluk").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    //            Burgermenuen er skjult på forsiden
+    document.querySelector("#burger").classList.toggle("hidden");
+
+    //            Hvis containerforside er synlig skal den skjules. Hvis den er skjult skal den vises
+    if (document.querySelector("#containerforside")) {
+        document.querySelector("#containerforside").classList.toggle("hidden");
+    }
+
+    //            Hvis menuknappen er synlig skal den skjules. Hvis den er skjult skal den vises
+    if (document.querySelector("#menuknap")) {
+        document.querySelector("#menuknap").classList.toggle("hidden");
+    }
+
+    //            Hvis splashen er synlig skal den skjules. Hvis den er skjult skal den vises
+    if (document.querySelector(".splash")) {
+        document.querySelector(".splash").classList.toggle("hidden");
+    }
+
 }
 
 async function hentNav() {
